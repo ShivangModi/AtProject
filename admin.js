e46ccfaaa05baddb0b1ef901c3b0280946b3a08a
ShivangModi <68642058+ShivangModi@users.noreply.github.com>
2020-08-10 07:30:53 +0530

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,8 +1,11 @@
 function validate() {
-    var aname = document.getElementById("aname");
-    var pass = documnet.getElementById("pass");
+    var aname = document.getElementById("aname").innerHTML;
+    var pass = documnet.getElementById("pass").innerHTML;
 
-    if (aname.value.trim() == "") {
+    if (aname == "user1" && pass == "user10702") {
+        alert("Welcome user1");
+        return true;
+    } else if (aname != "user1") {
         document.getElementById("l2").style.visibility = "hidden";
         document.getElementById("l1").style.visibility = "visible";
         return false;
@@ -10,8 +13,5 @@ function validate() {
         document.getElementById("l1").style.visibility = "hidden";
         document.getElementById("l2").style.visibility = "visible";
         return false;
-    } else {
-        alert("Welcome to Weddings.");
-        return true;
     }
 }
